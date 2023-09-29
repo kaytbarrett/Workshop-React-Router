98b456efe18549e1f3bd8eca327e86d824173506
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-09-28 18:25:51 -0600

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-
+    <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+    </div>
     </>
   )
 }
